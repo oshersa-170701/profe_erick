5bd87192b18bbea96518ebed2fc18c6f58441760
oshersa-170701 <oshersa170701@gmail.com>
2025-05-21 09:49:39 -0600

    MODIFICADO 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ProyectoNuevoGit';
+  constructor() {
+    console.log('FUNCIONES DE PRUEBA ')
+    console.warn('ADVERTENCIA')
+    console.log('PRUEBA DE CONFLICTOS')
+
+  }
 }
